0187200671eeb18827a425ec2dc388caf1610afe
Jordi Gasau <50402221+jordigasau@users.noreply.github.com>
2022-11-22 18:08:40 +0100

    todo

--- codi/components/ManipularArrays.vue
+++ codi/components/ManipularArrays.vue
@@ -9,29 +9,44 @@
                     v-model="temperatura"
                 ></v-text-field>
                 <v-btn @click="afegir()">afegir</v-btn>
+                <v-btn @dblclick="netejar()">netejar</v-btn>
+                <v-btn @mouseenter="borrarDarrerElement()">Borrar darrer</v-btn>
+                <v-btn @click="temperaturesAleatories()">Aleatori</v-btn>
+
             </v-col>
             <v-col cols="12" md="6" >
                 Resultat:
                 <br>
+                Mostres: {{tamanyArray}}
+                <br>
+                Mostres superiors a 50 graus: {{mesDe50}}
+                <br>
                 
                 <v-sparkline
                     :value="temperatures"
+                    :gradient="elMeuArrayDeColors"
+                 
                 ></v-sparkline>
-                <pre>
-                    {{temperatures}}
-                </pre>
-
-
+                <div v-for="element in temperatures">
+                    La temperatura és {{element}}
+                </div>
             </v-col>
         </v-row>
     </v-container>
 </template>
 <script>
+  
     export default{
+       mounted(){
+            // Inicialitzem l'array de temperatures
+            this.temperaturesAleatories()
+            
+       },
        data(){
             return{
+                elMeuArrayDeColors:['red', 'blue','yellow'],
                 temperatura:0,
-                temperatures:[33,24,41,23,44,11,23]
+                temperatures:[],
             }
        },
        methods:{
@@ -42,7 +57,61 @@
                 const tmp = parseFloat(this.temperatura)
                 console.log("El tipus de temperatura és",typeof(tmp))
                 this.temperatures.push(tmp)
+            },
+            netejar(){
+                console.log("netejant..")
+                this.temperatures = []
+                console.log(this.temperatures)
+            },
+            borrarDarrerElement(){
+                console.log("borrant últim element..")
+                this.temperatures.pop()
+            },
+            temperaturesAleatories(){
+                console.log("fent temperatures aleatories")
+
+                // Creem un array buid 
+                var nouArray = []
+                // repetim 100 vegades
+                for(var i = 0;i<100;i++){
+                    // afegim valors
+                    var valorAleatoriDecimals = Math.random()*100
+                    var valorAleatoriEnter = this.processarEnter(valorAleatoriDecimals)
+                    nouArray.push(valorAleatoriEnter)
+                }
+                // Assignem al array del component
+                this.temperatures = nouArray
+
+
+            },
+            processarEnter(val){
+                return parseInt(val)
             }
+       },
+       computed:{
+            tamanyArray(){
+                return this.temperatures.length
+            },
+            mesDe50(){
+                var mostresDeMesDe50Graus = 0
+                this.temperatures.forEach((temperatura)=>{
+                    if(temperatura>50){
+                        mostresDeMesDe50Graus++
+                    }
+                })
+                return mostresDeMesDe50Graus
+            },
+       },
+       watch:{
+        temperatura(newValue,oldValue){
+            console.log(newValue,oldValue)
+        },
+        mesDe50(newValue,oldValue){
+            if(newValue>55){
+                alert("ALERTA")
+            }
+        }
        }
+
     }
 </script>
---
+++ codi/components/Todo.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12" sm="6">
+                <v-textarea
+                label="Tasca"
+                placeholder="p.e: fer el llit" 
+                outlined>
+                </v-textarea>
+                <v-btn text>afegir tasca</v-btn>
+            </v-col>
+            <v-col cols="12" sm="6">
+                <v-row class="tasca" v-for="tasca in tasques">
+                    <v-col cols="8">
+                       <h2>{{tasca.text}}</h2>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-checkbox
+                            v-model="tasca.feta"
+                        ></v-checkbox>
+                    </v-col>
+                </v-row>
+                <pre>
+                    {{tasques}}
+                </pre>
+                
+            </v-col>   
+        </v-row> 
+    </v-container>
+</template>
+
+<script>
+export default{
+    data(){
+        return {       
+            tasques:[
+                {
+                    text:"Fer el llit",
+                    feta:true
+                },
+                {
+                    text:"Fer els deures",
+                    feta:false
+                },
+                {
+                    text:"Anar a comprar",
+                    feta:true
+                }]
+        }
+    }
+}
+</script>
+
+
+<style>
+    .tasca{
+        background-color: bisque;
+        margin-bottom: 10px;
+    }
+</style>
